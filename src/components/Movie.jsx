7554bc8d9386d1aee4fe3a8c128cc1ad0d0b10a7
Hasan YASEN <ysnhasan1@gmail.com>
2024-05-05 19:19:44 +0300

    Add bootstrap icon

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -112,8 +112,17 @@ function Movie(props) {
                                 </div>
 
                                 <div className="rating-and-video-container">
-                                    {movie.vote_average === 0 ? <span className="imdb-rating" style={{ color: "#ffffff99" }}>⭐ NR</span> :
-                                        <span className="imdb-rating">⭐ {(movie.vote_average?.toFixed(1))}</span>}
+                                    {movie.vote_average === 0 ?
+                                        <span className="imdb-rating" style={{ color: "#ffffff99" }}>
+                                            <i class="bi bi-star-fill bs-star-icon"></i>
+                                            <span>NR</span>
+                                        </span>
+                                        :
+                                        <span className="imdb-rating">
+                                            <i class="bi bi-star-fill bs-star-icon"></i>
+                                            <span>{(movie.vote_average?.toFixed(1))}</span>
+                                        </span>
+                                    }
 
                                     <span className="watch-video">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="white" d="M5.536 21.886a1.004 1.004 0 0 0 1.033-.064l13-9a1 1 0 0 0 0-1.644l-13-9A1 1 0 0 0 5 3v18a1 1 0 0 0 .536.886" /></svg>
