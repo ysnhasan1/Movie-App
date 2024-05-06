29496bf0f1069e23da46214694232a5924e97015
Hasan YASEN <ysnhasan1@gmail.com>
2024-05-06 19:02:12 +0300

    Update localStorage.js

--- src/localStorage/localStorage.js
+++ src/localStorage/localStorage.js
@@ -1,7 +1,7 @@
 export function fetchWhichMovies() {
     let which_movies = localStorage.getItem("which_movies")
     if (which_movies) {
-        return JSON.parse(which_movies)
+        return which_movies
     }
     else {
         return "top_rated"
@@ -9,13 +9,13 @@ export function fetchWhichMovies() {
 }
 
 export function storeWhichMovies(data) {
-    localStorage.setItem("which_movies", JSON.stringify(data))
+    localStorage.setItem("which_movies", data)
 }
 
 export function fetchSortedBy() {
     let sorted_by = localStorage.getItem("sorted_by")
     if (sorted_by) {
-        return JSON.parse(sorted_by)
+        return sorted_by
     }
     else {
         return "descending"
@@ -23,13 +23,13 @@ export function fetchSortedBy() {
 }
 
 export function storeSortedBy(data) {
-    localStorage.setItem("sorted_by", JSON.stringify(data))
+    localStorage.setItem("sorted_by", data)
 }
 
 export function fetchLanguage() {
     let language = localStorage.getItem("language")
     if (language) {
-        return JSON.parse(language)
+        return language
     }
     else {
         return "en-US"
@@ -37,13 +37,13 @@ export function fetchLanguage() {
 }
 
 export function storeLanguage(data) {
-    localStorage.setItem("language", JSON.stringify(data))
+    localStorage.setItem("language", data)
 }
 
 export function fetchInput() {
     let input = localStorage.getItem("input")
     if (input) {
-        return JSON.parse(input)
+        return input
     }
     else {
         return ""
@@ -51,5 +51,5 @@ export function fetchInput() {
 }
 
 export function storeInput(data) {
-    localStorage.setItem("input", JSON.stringify(data))
+    localStorage.setItem("input", data)
 }
