29111b6e1975e9fdb43841b708652596e5970d06
Hasan YASEN <ysnhasan1@gmail.com>
2024-09-13 20:40:40 +0300

    Improve code readability

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -28,39 +28,37 @@ function Images(props) {
         <>
             {(!loading_images && !loading_credits && !loading_movie) &&
                 <>
-                    {file_paths.length > 0 ? <div className="images-container">
-                        {language === "en-US" ?
-                            <h3>Backgrounds <span style={{ color: "#ffffff99" }}>({file_paths.length})</span></h3>
-                            :
-                            <h3>Arka Planlar <span style={{ color: "#ffffff99" }}>({file_paths.length})</span></h3>
-                        }
-                        <Carousel indicators={false}>
-                            {file_paths.map((path, index) => (
-                                <Carousel.Item key={index} interval={4000}>
-                                    <div className="img">
-                                        <LazyLoadImage
-                                            src={`https://image.tmdb.org/t/p/original/${path}`}
-                                            alt={`Slide ${index + 1}`}
-                                            placeholderSrc={posterPlaceholder}
-                                            effect="blur"
-                                            width="100%"
-                                            height="auto"
-                                            style={{ color: "white", aspectRatio: 3 / 2 }}
-                                        />
-                                    </div>
-                                </Carousel.Item>
-                            ))}
-                        </Carousel>
-                    </div>
+                    {file_paths.length > 0 ?
+                        <div className="images-container">
+                            <h3>
+                                {language === "en-US" ? "Backgrounds" : "Arka Planlar"}
+                                <span style={{ color: "#ffffff99" }}> ({file_paths.length})</span>
+                            </h3>
+                            <Carousel indicators={false}>
+                                {file_paths.map((path, index) => (
+                                    <Carousel.Item key={index} interval={4000}>
+                                        <div className="img">
+                                            <LazyLoadImage
+                                                src={`https://image.tmdb.org/t/p/original/${path}`}
+                                                alt={`Slide ${index + 1}`}
+                                                placeholderSrc={posterPlaceholder}
+                                                effect="blur"
+                                                width="100%"
+                                                height="auto"
+                                                style={{ color: "white", aspectRatio: 3 / 2 }}
+                                            />
+                                        </div>
+                                    </Carousel.Item>
+                                ))}
+                            </Carousel>
+                        </div>
                         :
                         <div className="images-container">
-                            {language === "en-US" ? <h3>Backgrounds</h3> : <h3>Arka Planlar</h3>}
+                            <h3>{language === "en-US" ? "Backgrounds" : "Arka Planlar"}</h3>
                             <div className="text-secondary">
-                                {language === "en-US" ?
-                                    <p>There are no background images for this movie.</p>
-                                    :
-                                    <p>Bu film için arka plan resmi yok.</p>
-                                }
+                                <p>
+                                    {language === "en-US" ? "There are no background images for this movie." : "Bu film için arka plan resmi yok."}
+                                </p>
                             </div>
                         </div>
                     }
