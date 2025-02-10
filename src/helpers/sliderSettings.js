75750808c4b7bea1ccf8492e48289a35e8252e4b
Hasan YASEN <ysnhasan1@gmail.com>
2025-02-10 23:10:31 +0300

    Improve Slick Slider UI and add a scrollbar for better user experience

--- src/helpers/sliderSettings.js
+++ src/helpers/sliderSettings.js
@@ -4,39 +4,32 @@ export const settings = {
     centerMode: false,
     speed: 1000,
     slidesToShow: 9,
-    slidesToScroll: 4,
     initialSlide: 0,
     autoplay: false,
-    arrows: true,
+    arrows: false,
     responsive: [
         {
             breakpoint: 1200,
             settings: {
-                slidesToShow: 7,
-                slidesToScroll: 3
+                slidesToShow: 7
             }
         },
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 5,
-                slidesToScroll: 2
+                slidesToShow: 5
             }
         },
         {
             breakpoint: 768,
             settings: {
-                slidesToShow: 4,
-                slidesToScroll: 4,
-                arrows: false
+                slidesToShow: 4
             }
         },
         {
             breakpoint: 600,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                arrows: false
+                slidesToShow: 3
             }
         }
     ]
