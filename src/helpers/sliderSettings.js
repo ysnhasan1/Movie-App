34f66989b0e561df0df2d93710927d57349b8d55
Hasan YASEN <ysnhasan1@gmail.com>
2025-02-10 23:23:33 +0300

    Fix bug

--- src/helpers/sliderSettings.js
+++ src/helpers/sliderSettings.js
@@ -13,28 +13,32 @@ export const settings = {
             breakpoint: 1200,
             settings: {
                 slidesToShow: 7,
-                slidesToScroll: 3
+                slidesToScroll: 3,
+                infinite: false,
             }
         },
         {
             breakpoint: 1024,
             settings: {
                 slidesToShow: 5,
-                slidesToScroll: 2
+                slidesToScroll: 2,
+                infinite: false,
             }
         },
         {
             breakpoint: 768,
             settings: {
                 slidesToShow: 4,
-                slidesToScroll: 4
+                slidesToScroll: 4,
+                infinite: false,
             }
         },
         {
             breakpoint: 600,
             settings: {
                 slidesToShow: 3,
-                slidesToScroll: 3
+                slidesToScroll: 3,
+                infinite: false,
             }
         }
     ]
